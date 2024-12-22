78f444697e9c00dd62db061ac25b21356f5462a9
weeris <wsupatgiat@gmail.com>
2024-12-22 22:39:10 +0700

    added predictors wrappers class

---
+++ tshybrid/predictors/wrappers.py
@@ -0,0 +1,13 @@
+from sklearn.base import BaseEstimator, RegressorMixin
+
+class SMFormulaWrapper(BaseEstimator, RegressorMixin):
+	""" A sklearn-style wrapper for formula-based statsmodels regressors """
+	def __init__(self, model_class, formula):
+		self.model_class = model_class
+		self.formula = formula
+	def fit(self, X, y=None):
+		self.model_ = self.model_class(self.formula, data=X)
+		self.results_ = self.model_.fit()
+	def predict(self, X):
+		return self.results_.predict(X)
+
