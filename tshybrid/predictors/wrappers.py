3969b9039638e007952c214e419001f390a549eb
weeris <wsupatgiat@gmail.com>
2024-12-22 23:53:14 +0700

    added statsmodel wrapper (unfinished)

--- tshybrid/predictors/wrappers.py
+++ tshybrid/predictors/wrappers.py
@@ -1,13 +1,34 @@
 from sklearn.base import BaseEstimator, RegressorMixin
 
-class SMFormulaWrapper(BaseEstimator, RegressorMixin):
-	""" A sklearn-style wrapper for formula-based statsmodels regressors """
-	def __init__(self, model_class, formula):
+class StatsmodelsWrapper(BaseEstimator, RegressorMixin):
+	def __init__(self, model_class, **parameters):
 		self.model_class = model_class
-		self.formula = formula
+		self.parameters = parameters
+
+		print(self.parameters)
+		self.init_params = dict()
+		self.fit_params = dict()
+
+
+	def set_params(self, **parameters):
+
+		for parameter, value in parameters.items():
+			if parameter.startswith('init__'):
+				self.init_params.update({parameter.lstrip('init__'): value})
+
+			elif parameter.startswith('fit__'):
+				self.init_params.update({parameter.lstrip('fit__'): value})
+
+			else:
+				setattr(self, parameter, value)
+
+		return self
+
 	def fit(self, X, y=None):
-		self.model_ = self.model_class(self.formula, data=X)
-		self.results_ = self.model_.fit()
+		self.model = self.model_class(endog=X, **self.init_params)
+		self.model_fit = self.model.fit(**self.fit_params)
+
 	def predict(self, X):
-		return self.results_.predict(X)
+		predictions = self.model_fit.predict(start=X.index[0], end=X.index[-1])
+		return predictions
 
